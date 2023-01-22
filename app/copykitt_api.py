bb1d113a327c35fc300060870d05b5e7767db1d6
Tanny <ryosuke.tanimura+dev@gmail.com>
2023-01-22 23:35:33 +0900

    Add copykitt api

---
+++ app/copykitt_api.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, Query
+
+from copykitt import generate_branding_snippet, generate_keywords
+
+app = FastAPI()
+
+MAX_INPUT_LENGTH = 32
+
+
+@app.get("/generate_snippet")
+async def generate_snippet_api(prompt: str = Query(max_length=MAX_INPUT_LENGTH)):
+    """Generate branding snippets related to the argument using openai."""
+
+    snippet = generate_branding_snippet(prompt)
+    return {"snippet": snippet}
+
+
+@app.get("/generate_keywords")
+async def generate_keywords_api(prompt: str = Query(max_length=MAX_INPUT_LENGTH)):
+    """Generate keywords related to the argument using openai."""
+
+    keywords = generate_keywords(prompt)
+    return {"keywords": keywords}
+
+
+@app.get("/generate_snippet_and_keywords")
+async def generate_snippet_and_keywords_api(
+    prompt: str = Query(max_length=MAX_INPUT_LENGTH),
+):
+    """Generate branding snippets and keywords related to the argument using openai."""
+
+    snippet = generate_branding_snippet(prompt)
+    keywords = generate_keywords(prompt)
+    return {"snippet": snippet, "keywords": keywords}
